08d52d7e24359f972bb1a7664afcb0afffd34511
vigneshcj001 <vigneshwarancj@gmail.com>
2025-02-02 19:17:50 +0530

    CJFoods

--- .history/src/__tests__/Contact.test_20250201143931.js
+++
@@ -1,8 +0,0 @@
-import ContactUs from "../pages/ContactUs";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-test("Should load ContactUs component", () => {
-  render(<ContactUs />);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
-});
